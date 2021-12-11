ee478c38ac55bc379d29151e2fb4697aa6d316ef
liadCohen29 <liadcohen2@gmail.com>
2021-12-11 15:41:15 +0200

    Builde week view page with mock data

--- src/Modules/MainPage.js
+++ src/Modules/MainPage.js
@@ -1,7 +1,60 @@
+import background from '../Assets/Images/background.jpg'
+import { Typography} from '@material-ui/core'
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import {useState} from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import WeekWeatherView from './WeekWeatherView';
+import FavoritePage from './FavoritePage';
+const AntTabs = withStyles({
+    root: {
+      borderBottom: '1px solid #e8e8e8',
+    },
+    indicator: {
+      backgroundColor: '#1890ff',
+    },
+  })(Tabs);
+  const AntTab = withStyles((theme) => ({
+    root: {
+      textTransform: 'none',
+      width: 40,
+      fontWeight: theme.typography.fontWeightRegular,
+     
+      '&:hover': {
+        color: '#40a9ff',
+        opacity: 1,
+      },
+      '&$selected': {
+        color: '#1890ff',
+        fontWeight: theme.typography.fontWeightMedium,
+      },
+      '&:focus': {
+        color: '#40a9ff',
+      },
+    },
+    selected: {},
+  }))((props) => <Tab disableRipple {...props} />);
 const MainPage =()=>{
+
+    const [tabValue, setValue] = useState('one');
+    const handleChange = (event,newValue) => {   
+        setValue(newValue);
+    };
+
+
     return(
         <div>
-            <h1>Hiii !!!</h1>
+            <img src={background} alt={'background'} class="img-responsive" style={{width: '99%', height: "99vh", overflow: "hidden", filter:'blur(6px)'}}/>
+            <div style={{position: "absolute", top: "1%", left: '1%',right:'1%',display: 'flex',
+                justifyContent: 'space-between', width:'99%'}}>
+            <Typography variant='h4' style={{paddingTop:'10px',color:'grey'}} >Herolo Weather App</Typography>
+                <AntTabs value={tabValue} onChange={handleChange} indicatorColor='primary'>
+                    <AntTab label="Home" value='one'/>
+                    <AntTab label="Favorites" value='two' />
+                </AntTabs>
+            </div>
+            {tabValue==='one'? <WeekWeatherView/> : <FavoritePage/>}  
+
         </div>
     )
 }
---
+++ src/Redux/actionTypes.js
@@ -0,0 +1,1 @@
+export const CITY_CLICKED = 'CITY_CLICKED';
